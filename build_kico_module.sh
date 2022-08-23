b74d360fb385b7fa2c9fb6ea02d0cc6c8ba3e273
Alexander Schulz-Rosengarten <als@informatik.uni-kiel.de>
2022-08-23 11:27:28 +0200

    Adjusted HG action workflow

--- build_kico_module.sh
+++ build_kico_module.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
 mvn clean package -pl org.lflang,org.lflang.ide,org.lflang.diagram,org.lflang.targetplatform
 mkdir tmp
-(cd tmp; unzip -uo ../org.lflang/target/org.lflang-0.1.0-SNAPSHOT.jar)
-(cd tmp; unzip -uo ../org.lflang.diagram/target/org.lflang.diagram-0.1.0-SNAPSHOT.jar)
+(cd tmp; unzip -uo ../org.lflang/target/org.lflang-0.3.1-SNAPSHOT.jar)
+(cd tmp; unzip -uo ../org.lflang.diagram/target/org.lflang.diagram-0.3.1-SNAPSHOT.jar)
 jar -cvf kico-lf.jar -C tmp .
